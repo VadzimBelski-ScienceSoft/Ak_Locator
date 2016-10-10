80b4bb8bea038e089f61f5b857f6c1d3c2d4524a
simone-cavanagh <simone@balanceinternet.com.au>
2016-10-10 11:23:11 +1100

    Upgraded Balance version to current Ak_Locator version from forked repository

--- src/app/code/community/Ak/Locator/Test/Model/Search.php
+++ src/app/code/community/Ak/Locator/Test/Model/Search.php
@@ -39,7 +39,7 @@ class Ak_Locator_Test_Model_Search extends EcomDev_PHPUnit_Test_Case
       */
     public function testLatLongSearchResultInstance()
     {
-        $params = array('lat'=>'-37.81420740.0000', 'long'=>'144.964045100000');
+        $params = array('lat'=>-37.814207400000, 'long'=>144.964045100000);
         $this->assertInstanceOf('Ak_Locator_Model_Resource_Location_Collection', $this->_model->search($params));
 
     }
@@ -53,4 +53,47 @@ class Ak_Locator_Test_Model_Search extends EcomDev_PHPUnit_Test_Case
         $this->assertInstanceOf('Ak_Locator_Model_Resource_Location_Collection', $this->_model->search($params));
     }
 
+    /**
+     * @test
+     * @loadFixture
+     */
+    public function testLatLongSearchDistance()
+    {
+        $params = array(
+            'lat'=>-37.814207400000,
+            'long'=>144.964045100000,
+            'distance' => 5
+        );
+
+        $this->assertEquals(2, $this->_model->search($params)->count());
+    }
+
+    /**
+     * @test
+     * @loadFixture testLatLongSearchDistance
+     */
+    public function testClosest()
+    {
+        $params = array(
+            'point'=> new Point(144.964045100000, -37.814207400000)
+        );
+
+        $this->assertEquals(1, $this->_model->search($params)->count());
+    }
+
+
+    /**
+     * @test
+     * @loadFixture
+     */
+    public function testLatLongSearchEnabled()
+    {
+        $params = array(
+            'lat'=>-37.814207400000,
+            'long'=>144.964045100000,
+            'distance' => 20
+        );
+
+        $this->assertEquals(2, $this->_model->search($params)->count());
+    }
 }
